7f43e2807702f4cbbe94d24b050466bbd25a781c
Vikashuvi <vikashuvi07@gmail.com>
2025-04-12 10:07:30 +0530

    profile added

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,34 @@ import Vegetables from './components/Vegetables'
 import Fruits from './components/Fruits'
 import Groceries from './components/Groceries'
 import Cart from './components/Cart'
+import Profile from './components/Profile'
 import { CartProvider } from './components/CartContext'
 import { SearchProvider } from './components/SearchContext'
+import { ProfileProvider } from './components/ProfileContext'
 import SearchResults from './components/SearchResults'
 
 function App() {
   return (
     <CartProvider>
       <SearchProvider>
-        <Router>
-          <div className="min-h-screen">
-            <Navbar />
-            <div className="pt-16"> {/* Add padding-top to account for fixed navbar */}
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/vegetables" element={<Vegetables />} />
-                <Route path="/fruits" element={<Fruits />} />
-                <Route path="/groceries" element={<Groceries />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/search" element={<SearchResults />} />
-              </Routes>
+        <ProfileProvider>
+          <Router>
+            <div className="min-h-screen">
+              <Navbar />
+              <div className="pt-16"> {/* Add padding-top to account for fixed navbar */}
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/vegetables" element={<Vegetables />} />
+                  <Route path="/fruits" element={<Fruits />} />
+                  <Route path="/groceries" element={<Groceries />} />
+                  <Route path="/cart" element={<Cart />} />
+                  <Route path="/search" element={<SearchResults />} />
+                  <Route path="/profile" element={<Profile />} />
+                </Routes>
+              </div>
             </div>
-          </div>
-        </Router>
+          </Router>
+        </ProfileProvider>
       </SearchProvider>
     </CartProvider>
   )
